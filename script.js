4af2a4b608283ade35dfcd5cf93b4387fe1fbaf5
Maira Silveira <silveirasmaira@gmail.com>
2022-08-03 18:33:58 -0300

    Correção de bugs

--- script.js
+++ script.js
@@ -13,34 +13,6 @@ btnDiv.addEventListener('click', function() {
   }
 });
 
-// Deck
-
-const listaSelecaoPokemons = document.querySelectorAll('.pokemon')
-const pokemonsCard = document.querySelectorAll('.cartao-lol')
-
-listaSelecaoPokemons.forEach(pokemon => {
-
-    pokemon.addEventListener('click', () => {
-        const cartaoPokemonAberto = document.querySelector('.aberto')        
-        cartaoPokemonAberto.classList.remove('aberto')
-
-        const idPokemonSelecionado = pokemon.attributes.id.value
-
-        const idDoCartaoPokemonParaAbrir = 'cartao-' + idPokemonSelecionado
-        const cartaoPokemonParaAbrir = document.getElementById(idDoCartaoPokemonParaAbrir)
-        cartaoPokemonParaAbrir.classList.add('aberto')
-
-        //remover a classe ativo que marca o pokémon selecionado
-        const pokemonAtivoNaListagem = document.querySelector('.ativo')
-        pokemonAtivoNaListagem.classList.remove('ativo')
-
-        //adicionar a classe ativo no item da lista selecionado 
-        const pokemonSelecionadoNaListagem = document.getElementById(idPokemonSelecionado)
-        pokemonSelecionadoNaListagem.classList.add('ativo')
-
-    })
-})
-
 // Carta de interrogação
 
 var carta0 = {
